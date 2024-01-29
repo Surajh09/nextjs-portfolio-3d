f8d92ae1dfb01a01dfc52eabcc5d779192242d6b
Surajh09 <98706142+Surajh09@users.noreply.github.com>
2024-01-29 18:15:57 +0530

    modified:   package-lock.json 	modified:   package.json 	deleted:    src/app/not-found.tsx 	new file:   src/app/not-found/not-found.module.css 	new file:   src/app/not-found/not-found.tsx

---
+++ src/app/not-found/not-found.tsx
@@ -0,0 +1,19 @@
+import { Title, Text, Button, Container, Group } from "@mantine/core";
+import classes from "./not-found.module.css";
+
+export function NotFoundTitle() {
+  return (
+    <Container className={classes.root}>
+      <div className={classes.label}>404</div>
+      <Title className={classes.title}>You have found a secret place.</Title>
+      <Text c="dimmed" size="lg" ta="center" className={classes.description}>
+        This place is either UNDER DEVELOPMENT or DOSE NOT EXIST.
+      </Text>
+      <Group justify="center">
+        <Button variant="subtle" size="md">
+          Take me back to home page
+        </Button>
+      </Group>
+    </Container>
+  );
+}
